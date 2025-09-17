568c97e8a8b0ed2a459fcb63f787957b835e4183
DavideFauri <davide.fauri@gmail.com>
2025-09-17 22:26:12 +0200

    Add machines/common.nix with sops-nix support for secrets

--- flake.nix
+++ flake.nix
@@ -4,7 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-#    hardware.url = "github:nixos/nixos-hardware"; #should I keep this? is it the unstable channel?
+    #    hardware.url = "github:nixos/nixos-hardware"; #should I keep this? is it the unstable channel?
+
+    # secrets management
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
@@ -12,48 +18,58 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: 
-  let
-    system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-
-      overlays = [
-        (final: _prev: { # allow the usage of pkgs.unstable.packagename, see https://www.reddit.com/r/NixOS/comments/1c6m5j4/how_to_use_both_stable_and_unstable_nixpkgs_in_a/l058ik3/
-          unstable = import nixpkgs-unstable { inherit (final) system config; };
-        })
-      ];
-    };
-
-  in {
-    
-    # MACHINES
-    nixosConfigurations = {
-
-      figaro = nixpkgs.lib.nixosSystem {
+      pkgs = import nixpkgs {
         inherit system;
-        
-        modules = [
-          {nixpkgs.hostPlatform = system;}
-          ./machines/common.nix
-          ./machines/figaro.nix
-        ];
-      };
-    };
-   
-    # USERS
-    homeConfigurations = {
-    
-      davide = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        config.allowUnfree = true;
 
-        modules = [
-          ./users/common.nix
-          ./users/davide.nix
+        overlays = [
+          (final: _prev: {
+            # allow the usage of pkgs.unstable.packagename, see https://www.reddit.com/r/NixOS/comments/1c6m5j4/how_to_use_both_stable_and_unstable_nixpkgs_in_a/l058ik3/
+            unstable = import nixpkgs-unstable { inherit (final) system config; };
+          })
         ];
       };
+
+    in
+    {
+
+      # MACHINES
+      nixosConfigurations = {
+
+        figaro = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; }; # so that I can use inputs in the modules (e.g. sops-nix)
+          inherit system;
+
+          modules = [
+            { nixpkgs.hostPlatform = system; }
+            ./machines/common.nix
+            ./machines/figaro.nix
+          ];
+        };
+      };
+
+      # USERS
+      homeConfigurations = {
+
+        davide = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            ./users/common.nix
+            ./users/davide.nix
+          ];
+        };
+      };
     };
-  };
 }
---
+++ machines/common.nix
@@ -0,0 +1,116 @@
+{ pkgs, inputs, ... }:
+
+{
+  nix.settings.experimental-features = [
+    "flakes"
+    "nix-command"
+  ];
+  nixpkgs.config.allowUnfree = true;
+
+  # ****** DEFAULT PACKAGES ******
+  environment.systemPackages = with pkgs; [
+    home-manager
+    git
+    sops # secrets management
+
+    htop
+    nethogs
+    tree
+    curl
+    tmux
+  ];
+
+  programs = {
+    fish.enable = true;
+  };
+
+  # ****** QT APPS DARK THEME ******
+  qt.enable = true;
+  qt.platformTheme = "qt5ct";
+  qt.style = "kvantum";
+
+  # ****** SECRETS ******
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    validateSopsFiles = false;
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; # auto import the hosts ssh key
+      #keyFile = "/private/sops/age/keys.txt"; # my private age key
+      #generateKey = true; # generate the above file if missing
+    };
+  };
+
+  # ****** USERS ******
+  users.users = {
+    davide = {
+      isNormalUser = true;
+      description = "Davide";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      shell = pkgs.fish;
+    };
+  };
+
+  nix.settings.trusted-users = [
+    "root"
+    "davide"
+  ];
+
+  # ****** SSH ACCESS ******
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+    };
+  };
+
+  # ****** AUTO MAINTENANCE ******
+
+  system.autoUpgrade = {
+    enable = true;
+    persistent = true;
+    dates = "weekly";
+    operation = "switch";
+    allowReboot = false;
+
+    flake = inputs.self.outPath;
+
+    flags = [
+      "--impure"
+      "--recreate-lock-file" # Deprecated, but maybe they introduce it again https://discourse.nixos.org/t/best-practices-for-auto-upgrades-of-flake-enabled-nixos-systems/31255/18
+    ];
+  };
+
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  # ****** LOCALE SETTINGS ******
+
+  time.timeZone = "Europe/Rome";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "it_IT.UTF-8";
+      LC_IDENTIFICATION = "it_IT.UTF-8";
+      LC_MEASUREMENT = "it_IT.UTF-8";
+      LC_MONETARY = "it_IT.UTF-8";
+      LC_NAME = "it_IT.UTF-8";
+      LC_NUMERIC = "it_IT.UTF-8";
+      LC_PAPER = "it_IT.UTF-8";
+      LC_TELEPHONE = "it_IT.UTF-8";
+      LC_TIME = "it_IT.UTF-8";
+    };
+  };
+
+}
