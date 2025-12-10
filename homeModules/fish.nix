78548c0564457b9745b5b38d145d17df73ccf2c2
DavideFauri <davide.fauri@gmail.com>
2025-12-10 22:47:33 +0100

    add command to full update

--- homeModules/fish.nix
+++ homeModules/fish.nix
@@ -27,6 +27,7 @@ in
       dali = "cd ${homeDir}/comfyUI; and env HSA_OVERRIDE_GFX_VERSION=11.0.0 nix shell ${comfyuiDir}#rocm.comfyui-with-extensions --command comfyui";
       warhol = "cd ${homeDir}/comfyUI; and nix run ${comfyuiDir}#rocm.krita-with-extensions";
       nolan = "RUSTICL_ENABLE=amdgpu DRI_PRIME=1 QT_QPA_PLATFORM=xcb davinci-resolve";
+      piano = "sudo nix flake update --flake .; and sudo nixos-rebuild switch --flake ./#(hostname) --impure ; and home-manager switch --flake ./#(whoami) --impure";
       #test = "sudo wg show-conf  | qrencode -t ansiutf8"
       #alert = "notify-send --urgency=low -i (if test $status -eq 0; echo terminal; else; echo error; end) (history | head -n1)"
     };
