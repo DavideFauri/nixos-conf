309b5220c5e331a033c558e2ffb6079518adcd58
DavideFauri <davide.fauri@gmail.com>
2026-05-09 16:44:08 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -24,12 +24,11 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      home-manager,
-      ...
+    { self
+    , nixpkgs
+    , nixpkgs-unstable
+    , home-manager
+    , ...
     }@inputs:
     let
       system = "x86_64-linux";
